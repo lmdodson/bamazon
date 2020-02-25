b21ac067a28b6cf899f42cbc4620fe365fa200ad
Leah Dodson <leahdodson9@gmail.com>
2020-02-25 16:04:03 -0600

    created the starting database for the bamazon store and added dependency for mysql

---
+++ bamazon_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("PlayStation 4 Pro", "Electronics", 299.98, 10), ("Instant Pot", "Kitchen", 59.99, 2), 
+("Learning Web App Development", "Books", 30.99, 7), ("Play-Doh Play Pack", "Toys", 5.49, 3),
+("Red Bull 100pk", "Beverages", 259.59, 2), ("Samsung TV", "Electronics", 499.95, 2),
+("Ninja 250r Motorcycle", "Vehicles", 1525.00, 1), ("Frozen 2 DVD", "Movies", 24.99, 100),
+("Castle in Ireland", "Outrageous Things", 99999999.99, 2),("Exotic Alien Animal from Mars", "Outrageous Things", 9999.99, 4);
